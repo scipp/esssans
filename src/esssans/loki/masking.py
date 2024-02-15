3658ebeb8f0fc027c5136158658ad94d4c33c8c2
Neil Vaytet <neil.vaytet@esss.se>
2024-02-15 13:43:45 +0100

    use the max of integrated counts to define masking threshold instead of logical sizes

--- src/esssans/loki/masking.py
+++ src/esssans/loki/masking.py
@@ -26,25 +26,23 @@ DetectorTubeEdgeMask = NewType('DetectorTubeEdgeMask', sc.Variable)
 
 
 def detector_straw_mask(
-    sample_straws: RawData[SampleRun],
+    data: RawData[SampleRun],
 ) -> DetectorLowCountsStrawMask:
     """
     This mask aims to remove straws with low counts. It is based on the assumption
-    that the integrated counts in a straw should be at least greater than the number of
-    pixels in the straw (within a fudge factor of 0.5).
+    that the integrated counts in a straw should be at least greater than a fraction of
+    the maximum integrated counts in a straw (fraction set to 1% here).
     This is not true for straws that are not hit by the beam.
     Because the straws are horizontal, this mask will typically mask straws that are
     at the top and bottom edges of the detector panel.
     Note that this has only been tested on the main rear detector panel.
     """
-    return DetectorLowCountsStrawMask(
-        sample_straws.sum('pixel').data
-        < sc.scalar(sample_straws.sizes['pixel'] * 0.5, unit='counts')
-    )
+    pixel_sum = data.sum('pixel').data
+    return DetectorLowCountsStrawMask(pixel_sum < pixel_sum.max() * 0.01)
 
 
 def detector_beam_stop_mask(
-    sample_straws: RawData[SampleRun],
+    data: RawData[SampleRun],
     beam_stop_position: BeamStopPosition,
     beam_stop_radius: BeamStopRadius,
 ) -> DetectorBeamStopMask:
@@ -55,28 +53,25 @@ def detector_beam_stop_mask(
     data as an input). Therefore, only a rough estimate of the beam position is used.
     Note that this has only been tested on the main rear detector panel.
     """
-    pos = sample_straws.coords['position'] - beam_stop_position
+    pos = data.coords['position'] - beam_stop_position
     pos.fields.z *= 0.0
     return DetectorBeamStopMask((sc.norm(pos) < beam_stop_radius))
 
 
 def detector_tube_edge_mask(
-    sample_straws: RawData[SampleRun],
+    data: RawData[SampleRun],
 ) -> DetectorTubeEdgeMask:
     """
     This mask aims to remove the left and right edges of the detector panel.
     Those areas typically have low counts. This mask is based on the assumption
     that the counts integrated inside a vertical slab (summed over the ``straw`` and
-    ``layer`` dimensions) should be at least greater that the number of pixels that
-    make up the slab (within a fudge factor of 0.5).
+    ``layer`` dimensions) should be at least greater that a fraction of
+    the maximum integrated counts in a slab (fraction set to 5% here).
     This is not true for slabs that are not hit by the beam.
     Note that this has only been tested on the main rear detector panel.
     """
-    other_dims = set(sample_straws.dims) - {'pixel'}
-    size = np.prod([sample_straws.sizes[dim] for dim in other_dims])
-    return DetectorTubeEdgeMask(
-        sample_straws.sum(other_dims).data < sc.scalar(size * 0.5, unit='counts')
-    )
+    other_dims_sum = data.sum(set(data.dims) - {'pixel'}).data
+    return DetectorTubeEdgeMask(other_dims_sum < other_dims_sum.max() * 0.05)
 
 
 def mask_detectors(
