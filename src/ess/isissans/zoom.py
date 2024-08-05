747f9fb6393706f61488b82593f2b2cf0b55345f
Simon Heybrock <simon.heybrock@ess.eu>
2024-08-05 06:42:27 +0200

    Revert Zoom workflows to functions

--- src/ess/isissans/zoom.py
+++ src/ess/isissans/zoom.py
@@ -1,11 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
-from typing import Any
-
 import sciline
-from ess.reduce.workflow import register_workflow
 from ess.sans import providers as sans_providers
-from ess.sans.workflow import SANSWorkflow
 
 from .data import load_tutorial_direct_beam, load_tutorial_run
 from .general import default_parameters
@@ -23,35 +19,27 @@ def set_mantid_log_level(level: int = 3):
         pass
 
 
-@register_workflow
-class ZoomWorkflow(SANSWorkflow):
+def ZoomWorkflow() -> sciline.Pipeline:
     """Create Zoom workflow with default parameters."""
+    from . import providers as isis_providers
 
-    def __init__(self):
-        from . import providers as isis_providers
+    set_mantid_log_level()
 
-        set_mantid_log_level()
-
-        params = default_parameters()
-        zoom_providers = sans_providers + isis_providers + mantid_providers
-        pipeline = sciline.Pipeline(providers=zoom_providers, params=params)
-        pipeline.insert(read_xml_detector_masking)
-        super().__init__(pipeline)
-
-    def _default_param_values(self) -> dict[sciline.typing.Key, Any]:
-        return default_parameters()
+    params = default_parameters()
+    zoom_providers = sans_providers + isis_providers + mantid_providers
+    workflow = sciline.Pipeline(providers=zoom_providers, params=params)
+    workflow.insert(read_xml_detector_masking)
+    return workflow
 
 
-@register_workflow
-class ZoomTutorialWorkflow(ZoomWorkflow):
+def ZoomTutorialWorkflow() -> sciline.Pipeline:
     """
     Create Zoom tutorial workflow.
 
     Equivalent to :func:`ZoomWorkflow`, but with loaders for tutorial data instead
     of Mantid-based loaders.
     """
-
-    def __init__(self):
-        super().__init__()
-        self.pipeline.insert(load_tutorial_run)
-        self.pipeline.insert(load_tutorial_direct_beam)
+    workflow = ZoomWorkflow()
+    workflow.insert(load_tutorial_run)
+    workflow.insert(load_tutorial_direct_beam)
+    return workflow
